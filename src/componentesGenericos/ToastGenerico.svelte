412f3f4f136863784d6c3e964112d1aed3e1cce1
Alfonso Chafla <117238753+Luden20@users.noreply.github.com>
2025-07-01 20:56:32 -0500

    Update ToastGenerico.svelte

--- src/componentesGenericos/ToastGenerico.svelte
+++ src/componentesGenericos/ToastGenerico.svelte
@@ -1,11 +1,20 @@
 <script>
 	import { toasts } from 'svelte-toasts';
+	import { tick } from 'svelte';
 	export let mensaje;
 	export let exito;
 
-	$: if (mensaje && exito !== undefined) {
-		const esExito = exito === true || exito === 'Exito' || exito === 'true';
+	let trigger = 0;
 
+	$: trigger, showToast(); // se ejecuta cada vez que trigger cambie
+
+	// Fuerza reinicio si se pasa el mismo mensaje/exito
+	$: if (mensaje || exito !== undefined) {
+		trigger += 1;
+	}
+
+	function showToast() {
+		const esExito = exito === true || exito === 'Exito' || exito === 'true';
 		toasts.add({
 			title: esExito ? 'Éxito' : 'Error',
 			description: mensaje,
